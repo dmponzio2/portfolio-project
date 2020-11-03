8a1e9d3dcbf0530ed87b148272392f5543f1240d
David <davidnj20@yahoo.com>
2020-11-02 23:27:59 -0500

    Added Bootstrap and jobs are showing up

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+
+# Add the Blog app to the settings
+
+# Create a migrations
+
+# Migrate
+
+# Add to the admin
